61012212f87bc04a45c00591ba6c4627f5af81c0
RegesDias <regesfd@gmail.com>
2020-09-10 17:13:04 -0300

    get

--- aprendendo-laravel/database/migrations/2020_09_10_182613_create_filmes_table.php
+++ aprendendo-laravel/database/migrations/2020_09_10_182613_create_filmes_table.php
@@ -3,9 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Validation\Rules\Unique;
 
-class CreateClientesTable extends Migration
+class CreateFilmesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,11 +13,8 @@ class CreateClientesTable extends Migration
      */
     public function up()
     {
-        Schema::create('clientes', function (Blueprint $table) {
+        Schema::create('filmes', function (Blueprint $table) {
             $table->id();
-            $table->string('nome',150);
-            $table->string('image',100)->nullable()->comment('imagem');
-            $table->string('cpf-cnpj', 19)->unique();
             $table->timestamps();
         });
     }
@@ -30,6 +26,6 @@ class CreateClientesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('clientes');
+        Schema::dropIfExists('filmes');
     }
 }
